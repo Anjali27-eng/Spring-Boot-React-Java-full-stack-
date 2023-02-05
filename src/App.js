a90344f9b068fc3a179a222ac35314796a0c9e14
AR <rajoleanjali04@gmail.com>
2023-02-05 18:51:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import EmployeeRegistation from './components/EmployeeRegistaion';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <EmployeeRegistation/>
+    </div>
+  );
+}
+
+export default App;
